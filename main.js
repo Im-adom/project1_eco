c2728c01cad8560c7dd5c56026195d8c84556435
Im-adom <graceamaningkwarteng@gmail.com>
2024-11-06 23:28:54 +0000

    Beginning phase of take home assignment_ecoriseproject

---
+++ main.js
@@ -0,0 +1,9 @@
+//const imgCont = document.querySelector('.imgcont');
+
+//imgCont.addEventListener('dragstart', function() {
+   // imgCont.classList.add('animate'); // Triggers the animation
+//});
+
+//imgCont.addEventListener('dragend', function() {
+    //imgCont.classList.remove('animate'); // Removes the animation when drag ends
+//});
